8c6251dd46192e075c2c624480bb853a98a71fa5
Jan Rak <janrak@MacBookPro16.local>
2023-11-17 14:45:52 +0100

    Jan's improvement and fit in range

--- model.cpp
+++ model.cpp
@@ -1,61 +1,61 @@
 void model() {
-	// Measurement 503
-	// double tempAmbHeating = 20.69;
-	// double tempAmbCooling = 38.93;
-	// double coolingStartTime = 5180.0;
-	// int coolingStartIndex = 1030;
-	// int heatingEndIndex = 970;
-	// TString filename = "measurement_503.csv";
+    // Measurement 503
+    // double tempAmbHeating = 20.69;
+    // double tempAmbCooling = 38.93;
+    // double coolingStartTime = 5180.0;
+    // int coolingStartIndex = 1030;
+    // int heatingEndIndex = 970;
+    // TString filename = "measurement_503.csv";
 
-	// Measurement 509
-	double tempAmbHeating = 21.63;
-	double tempAmbCooling = 32.0;
-	double coolingStartTime = 14205.0;
-	int coolingStartIndex = 2823;
-	int heatingEndIndex = 1449;
-	TString filename = "measurement_509.csv";
+    // Measurement 509
+    double tempAmbHeating = 21.63;
+    double tempAmbCooling = 32.0;
+    double coolingStartTime = 14205.0;
+    int coolingStartIndex = 2823;
+    int heatingEndIndex = 1449;
+    TString filename = "measurement_509.csv";
 
-	TF1 *f;
-	TF1 *fout = new TF1("fout","[0]", 0, 40);
+    TF1 *f;
+    TF1 *fout = new TF1("fout","[0]", 0, 40);
 
-	ifstream myfile(filename);
-	if(myfile.fail()) {
-		cout << "File does not exists" << endl;
-		exit(0);
-	}
+    ifstream myfile(filename);
+    if(myfile.fail()) {
+        cout << "File does not exists" << endl;
+        exit(0);
+    }
 
-	char dummyText[1024];
-	myfile.getline(dummyText, 1024);
+    char dummyText[1024];
+    myfile.getline(dummyText, 1024);
 
-	double pwr, tvd02, tvd06, tvd03, pt1000, clock;
+    double pwr, tvd02, tvd06, tvd03, pt1000, clock;
 
     TGraph *chartHeating = new TGraph();
-	TGraph *chartCooling = new TGraph();
-	TGraph *gr;
+    TGraph *chartCooling = new TGraph();
+    TGraph *gr;
 
     fout->SetLineColor(1); fout->SetLineStyle(2); fout->Draw("same");
 
-	cout << "Starting to read!" << endl;
-	int count = 0;
-	while(!myfile.eof()) {
-		//myfile >> pwr >> tvd02 >> tvd06 >> tvd03 >> pt1000 >> clock;
-		myfile >> pwr >> tvd02 >> tvd06 >> pt1000 >> clock;
+    cout << "Starting to read!" << endl;
+    int count = 0;
+    while(!myfile.eof()) {
+        //myfile >> pwr >> tvd02 >> tvd06 >> tvd03 >> pt1000 >> clock;
+        myfile >> pwr >> tvd02 >> tvd06 >> pt1000 >> clock;
 
-		if(count <= heatingEndIndex) {
-			chartHeating->AddPoint(clock, tvd02 - tempAmbHeating);
-		} else if(count > coolingStartIndex && count < 10000) {
-			chartCooling->AddPoint(clock - coolingStartTime, tvd02 - tempAmbCooling);
-		}
-		
-		count++;
-	}
+        if(count <= heatingEndIndex) {
+            chartHeating->AddPoint(clock, tvd02 - tempAmbHeating);
+        } else if(count > coolingStartIndex && count < 10000) {
+            chartCooling->AddPoint(clock - coolingStartTime, tvd02 - tempAmbCooling);
+        }
 
-	cout << "Read count: " << count << endl;
+        count++;
+    }
 
-	TH2F *hfr;
+    cout << "Read count: " << count << endl;
+
+    TH2F *hfr;
 
     // First heating chart
-	mc(2,1.8, 300, 100, 10, 2.);
+    mc(2, 1.8, 300, 100, 10, 2.);
     gStyle->SetOptStat(0);
     gStyle->SetOptTitle(0);
     gStyle->SetMarkerSize(1.6);
@@ -74,36 +74,37 @@ void model() {
     gr->Draw();
 
     // First cooling chart
-	mc(3,1.8, 300, 100, 10, 2.);
+    mc(3,1.8, 300, 100, 10, 2.);
     gStyle->SetOptStat(0);
     gStyle->SetOptTitle(0);
     gStyle->SetMarkerSize(1.6);
     mpad->SetLogx(0);
     mpad->SetGridx(0);
-    mpad->SetLogy(0);
+    mpad->SetLogy(1);
     mpad->SetGridy(0);
     gPad->SetLeftMargin(0.17);
 
-    hfr = new TH2F("hfr"," ", 10, 0, 20, 10, 0, 100);
+    gr=chartCooling;
+
+    double Xrange[2] = {gr->GetXaxis()->GetXmin(), gr->GetXaxis()->GetXmax()};
+    double Yrange[2] = {gr->GetYaxis()->GetXmin(), gr->GetYaxis()->GetXmax()};
+
+    hfr = new TH2F("hfr"," ", 10, Xrange[0], Xrange[1], 10, Yrange[0], Yrange[1]); 
     hset( *hfr, "Time [s]","Temp [deg]");
     hfr->Draw();
 
-    gr=chartCooling;
     gr->SetMarkerStyle(20); 
     gr->SetMarkerColor(4); 
     gr->SetMarkerSize(0.4); 
     gr->SetLineWidth(4); 
     gr->SetLineColor(4); 
-    gr->Draw();
+    gr->Draw("same");
 
-    // TF1 *fexp = new TF1("fexp","[0]+[1]*exp(-x[2])", 100, 2500);
-    // fexp->SetParameters(1.444, 5.2, 0.0041);
-    // TLine *l = new TLine(100,1.444,2500,1.444); l->SetLineWidth(2); l->SetLineColor(4); l->SetLineStyle(2); l->Draw();
-    // fexp->Draw("same");
-    // gr->Fit("fexp");
-    // gr->SetMarkerStyle(43); 
-    // gr->SetMarkerSize(1.6); 
-    // gr->Draw();
+    TF1 *fexp = new TF1("fexp","[0]*exp(x*[1])", Xrange[0], Xrange[1]);
+    fexp->SetParameters(3.85653, -6.28243e-05);
+    fexp->SetLineWidth(3);
+    //fexp->Draw("same");
+    gr->Fit("fexp"," ","R", 5000, 30000);
 
     f = fout;
     f->SetLineColor(1); 
